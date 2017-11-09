b8bd0c16fa411bde43839fa099bab64580035f3c
zpf <zhaipengfei@ict.ac.cn>
2017-11-09 09:54:45 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ri5cy_gnu_toolchain
 old version of riscv toolchain
+
+use this toolchain should set the following env varibles:
+
+export RISCV32_TOOLCHAIN_PATH=/home/pengfei/ri5cy_gnu_toolchain/install
+export ZEPHYR_GCC_VARIANT=riscv32
+
+then source zephyr-env.sh to set zephyr env varibles.
+
---
+++ install/lib/gcc/riscv32-unknown-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/fyh/riscv/ri5cy_gnu_toolchain/build/src/newlib-gcc/configure --target=riscv32-unknown-elf --prefix=/home/fyh/riscv/ri5cy_gnu_toolchain/install --disable-shared --disable-threads --enable-tls --enable-languages=c,c++ --with-newlib --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-multilib --with-arch=IM";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "IM" } };
